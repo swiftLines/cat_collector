20bd7da5a625514e33650a05b3cbbbf59c32c64f
Jeremy Underwood <underwood.j248@gmail.com>
2024-05-17 16:25:20 -0400

    Define cat url, view and template. Add images and css.

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # route for home page
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    # route for about page
+    path('about/', views.about, name='about'),
+    # route for cats index
+    path('cats/', views.cats_index, name='cats_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Add imports
 from django.http import HttpResponse
+from main_app.models import cats
 
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
+# Render the about view
 def about(request): 
     return render(request, 'about.html')
+
+# Render the cats_index view
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
