07b9d1aa2e120c44646b93f141c6718b76fddbe2
Jeremy Underwood <underwood.j248@gmail.com>
2024-05-18 18:12:00 -0400

    add home view and template plus cat Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 
 # Create your models here.
-# Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 
 # Add imports
 from django.http import HttpResponse
-from main_app.models import cats
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 # Render the about view
 def about(request): 
@@ -15,3 +14,18 @@ def about(request):
 # Render the cats_index view
 def cats_index(request):
     return render(request, 'cats/index.html', {'cats': cats})
+
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
