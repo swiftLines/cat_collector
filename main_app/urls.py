6dad92ab0bffda8939d4d2d264989fbd792d105e
Jeremy Underwood <underwood.j248@gmail.com>
2024-06-14 18:00:29 -0400

    Create class based view to show a form and create a cat.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # route for cats index
     path('cats/', views.cats_index, name='cats_index'),
     path('cats/<int:cat_id>/', views.cats_detail, name='cats_detail'),
+    # route to show a form and create a cat
+    path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Cat
 # Add imports
 from django.http import HttpResponse
@@ -7,19 +8,27 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
+
 # Render the about view
 def about(request): 
     return render(request, 'about.html')
+
 
 # Render the cats_index view
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
+
 # Render cats_detail view
 def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', { 'cat': cat})
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
 
 
 def __init__(self, name, breed, description, age):
